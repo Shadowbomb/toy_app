04464d3e17df1179d9101d74f0fe01aea02e0490
Shadowbomb <shadowbomb20@gmail.com>
2018-09-13 03:15:47 +0000

    Fixed :body issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
-class Micropost < ApplicationRecord
-  belongs_to :user
-  validates :content, length: { maximum: 140 },
-                      presence: true
+class User < ApplicationRecord
+  has_many :microposts
+  validates :body, presence: true
+  validates :body, presence: true
 end
